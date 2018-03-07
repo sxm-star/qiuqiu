61e4ad570c3cf1d7a878a671d8047dcc13424dc0
xuanmings <18720987043@139.com>
2018-03-07 21:52:01 +0800

    init ...

--- taotao-manage/taotao-manage-mapper/src/main/java/com/taotao/manage/mapper/ItemParamItemMapper.java
+++
@@ -1,8 +0,0 @@
-package com.taotao.manage.mapper;
-
-import com.taotao.manage.base.mapper.TaotaoMapper;
-import com.taotao.manage.pojo.ItemParamItem;
-
-public interface ItemParamItemMapper extends TaotaoMapper<ItemParamItem> {
-
-}
